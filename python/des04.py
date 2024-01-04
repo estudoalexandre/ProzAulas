f2b4d50a52ee33dc3dbf312b90a92c429f8deef5
Alexandre Oliveira <107891784+estudoalexandre@users.noreply.github.com>
2024-01-04 17:24:52 -0300

    adicionando enunciado desenvolvimento 4.

--- python/des04.py
+++ python/des04.py
@@ -1,3 +1,11 @@
+# Faça uma função calculadora de dois números com três parâmetros: os dois primeiros serão os números da operação e o terceiro será a entrada que definirá a operação a ser executada. Considera a seguinte definição:
+# 1. Soma
+# 2. Subtração
+# 3. Multiplicação
+# 4. Divisão
+
+# Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.
+
 def calculadora(operacao, x, y):
     if operacao == '+':
        return x + y
@@ -8,7 +16,7 @@ def calculadora(operacao, x, y):
     elif operacao == '/':
        return x / y
     else:
-        return "Voce precisa digitar um Operador Valido"
+        return 0
     
 calculo = calculadora('*', 10, 20)
 print(calculo)
