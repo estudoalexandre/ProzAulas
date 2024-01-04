86a2fc9224c5e7f001cb78958179b6ed6a6d4fe2
Alexandre Oliveira <107891784+estudoalexandre@users.noreply.github.com>
2024-01-04 15:23:05 -0300

    Proz

---
+++ python/des03.py
@@ -0,0 +1,14 @@
+
+## Usando for
+# for andar in range(1, 21):
+#         if andar != 13:
+#                 print(andar)
+                
+
+## Usando While
+andar = 20
+
+while andar >= 1:
+    if andar != 13:
+        print(andar)
+    andar -= 1
