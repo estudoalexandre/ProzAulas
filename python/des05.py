99d3bd6d670b4d587727d9c53135de981fc0fb55
Alexandre Oliveira <107891784+estudoalexandre@users.noreply.github.com>
2024-01-04 18:35:21 -0300

    Desenvolvimento 5 Proz, calculadora usuario

--- python/des05.py
+++ python/des05.py
@@ -13,4 +13,24 @@ def calculadora():
             break
         elif operacao in ('1', '2', '3', '4'):
             x = input('Digite o primeiro valor: ')
-            y = input('Digite o segundo Valor')
+            y = input('Digite o segundo Valor: ')
+            
+            if operacao == '1':
+                resultado = float(x) + float(y)
+            elif operacao == '2':
+                resultado = float(x) - float(y)
+            elif operacao == '3':
+                resultado = float(x) * float(y)
+            elif operacao == '4':
+                if float(y) != 0:
+                    resultado = float(x) / float(y)
+                else:
+                    print("ERROR Divisao por Zero.")
+                    continue
+                
+            print("Resultado:", resultado)
+        else:
+            print("Essa opcao nao existe. Por favor, escolha novamente a opcao certa")
+
+calculadora()
+                
