a4337af9180071522f74551971e74e74987eb464
Alexandre Oliveira <alexandresousaa70@gmail.com>
2024-01-17 16:03:55 -0300

    treinando funções basicas de calculo de salario hora

--- python/ex01.py
+++ python/ex01.py
@@ -9,30 +9,41 @@
 # print("A temperatura em Fahrenheit é:", fahrenheit)
     
     
-def graus_fahrenheit(graus):
-    fahrenheit = (graus * 9/5) + 32
-    return f"A temperatura em Fahrenheit é: {fahrenheit}"
+# def graus_fahrenheit(graus):
+#     fahrenheit = (graus * 9/5) + 32
+#     return f"A temperatura em Fahrenheit é: {fahrenheit}"
 
-temperatura_celsius = int(input("Digite a temperatura: "))
-temperatura_fahrenheit = graus_fahrenheit(temperatura_celsius)
-print(f"A temperatura em Fahrenheit é de: {temperatura_fahrenheit}\n")
-
-
-def lista_compras():
-    lista = []
-    print('Após terminar sua lista, apenas digite: "sair"')
-    while True:
-        item = input("Digite um item para adicionar a lista de compras: ")
-        if item == "sair":
-            break
-        else:
-            lista.append(item)
-    return lista
+# temperatura_celsius = int(input("Digite a temperatura: "))
+# temperatura_fahrenheit = graus_fahrenheit(temperatura_celsius)
+# print(f"A temperatura em Fahrenheit é de: {temperatura_fahrenheit}\n")
 
 
-lista_de_compras = lista_compras()
-print("Lista de compras: ")
-for item in lista_de_compras:
-    print("Produto:" + " - " + item)
+# def lista_compras():
+#     lista = []
+#     print('Após terminar sua lista, apenas digite: "sair"')
+#     while True:
+#         item = input("Digite um item para adicionar a lista de compras: ")
+#         if item == "sair":
+#             break
+#         else:
+#             lista.append(item)
+#     return lista
+
+
+# lista_de_compras = lista_compras()
+# print("Lista de compras: ")
+# for item in lista_de_compras:
+#     print("Produto:" + " - " + item)
             
     
+
+
+def salario_hora():
+    salario = float(input("Insirá o salario Mensal: "))
+    hora = float(input("Insira suas horas mensais: "))
+    
+    return salario / hora
+
+
+resultado = salario_hora()
+print(f"Salário por hora: R${resultado:.2f}")
