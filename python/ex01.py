d85891e63b19a841e70bd7f4ae0bffee3f4f1963
Alexandre Oliveira <alexandresousaa70@gmail.com>
2024-01-17 15:19:14 -0300

    acrescentando codigo dentro de função para treino

--- python/ex01.py
+++ python/ex01.py
@@ -1,11 +1,19 @@
-for i in range(100):
-    print("Hello World")
+# for i in range(100):
+#     print("Hello World")
     
     
     #Praticando
     
-    celsius = int(input("Digite a temperatura em Graus: "))
-    fahrenheit = (celsius * 9/5) + 32
+# celsius = int(input("Digite a temperatura em Graus: "))
+# fahrenheit = (celsius * 9/5) + 32    
+# print("A temperatura em Fahrenheit é:", fahrenheit)
     
-    print("A temperatura em Fahrenheit é:", fahrenheit)
+    
+def graus_fahrenheit(graus):
+    fahrenheit = (graus * 9/5) + 32
+    return f"A temperatura em Fahrenheit é: {fahrenheit}"
+
+temperatura_celsius = int(input("Digite a temperatura: "))
+temperatura_fahrenheit = graus_fahrenheit(temperatura_celsius)
+print(f"A temperatura em Fahrenheit é de: {temperatura_fahrenheit}")
     
