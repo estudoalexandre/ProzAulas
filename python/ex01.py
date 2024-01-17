85ef9d17f97f80beed6e5494a962380cb5689ddb
Alexandre Oliveira <alexandresousaa70@gmail.com>
2024-01-17 15:07:33 -0300

    aprendendo calcular temperatura com python

--- python/ex01.py
+++ python/ex01.py
@@ -1,2 +1,11 @@
 for i in range(100):
     print("Hello World")
+    
+    
+    #Praticando
+    
+    celsius = int(input("Digite a temperatura em Graus: "))
+    fahrenheit = (celsius * 9/5) + 32
+    
+    print("A temperatura em Fahrenheit é:", fahrenheit)
+    
