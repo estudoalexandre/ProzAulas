0a711097b12d5b9bc5d97ff51b45dc3908b4b43f
Alexandre Oliveira <alexandresousaa70@gmail.com>
2024-04-12 16:47:58 -0300

    Atividade JS I Modulo 3.2

---
+++ proz-html/script.js
@@ -0,0 +1,8 @@
+const titulo = document.getElementById("titulo");
+const listaNaoOrdenada = document.querySelector("ul");
+const link = document.getElementById("link");
+const listaOrdenada = document.getElementById("lista-ordenada");
+
+
+titulo.innerText = "Título da Página";
+link.innerText = "Visite o site da Proz Educação";  
